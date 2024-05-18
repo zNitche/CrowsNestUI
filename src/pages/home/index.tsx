22e9621062911ec959948d1d7c3dbab6285d19b9
zNitche <53448802+zNitche@users.noreply.github.com>
2024-05-18 19:37:20 +0200

    mock home page implementation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,26 @@
+import HeaderBar from "@/components/header_bar/header_bar";
 import classes from "./index.module.css";
+import { useState } from "react";
+import ServiceTile from "@/components/service_tile/service_tile";
 
 export default function Home() {
-    return <div className={classes.wrapper}>Home</div>;
+    const [searchQuery, setSearchQuery] = useState("");
+
+    return (
+        <div className={classes.wrapper}>
+            <HeaderBar
+                searchValue={searchQuery}
+                setSearchValue={setSearchQuery}
+                envData={{ temperature: 24, humidity: 65 }}
+            />
+            <div className={classes["content-container"]}>
+                <ServiceTile
+                    name={"ao3"}
+                    addedAt={new Date().toISOString()}
+                    redirectUrl={"#"}
+                    isAvailable={true}
+                />
+            </div>
+        </div>
+    );
 }
