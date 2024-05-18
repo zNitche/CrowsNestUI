d02fb9619f08487bc340d7dabe0213208aa51e8b
zNitche <53448802+zNitche@users.noreply.github.com>
2024-05-18 21:03:40 +0200

    tweaked build preparation script for proper favicon handling

--- prepare_build.py
+++ prepare_build.py
@@ -1,15 +1,21 @@
 import os
+import shutil
 import re
 
 
-def compress_files(root_dir, file_paths):
+def process_files(root_dir, file_paths):
     for file_path in file_paths:
         path = f"{root_dir}{file_path}"
 
         print(f"Processing '{path}'...")
 
-        if os.path.exists(path) and not path.endswith(".gz"):
-            os.system(f"gzip {path}")
+        if os.path.exists(path):
+            if "favicon.svg" in file_path:
+                shutil.move(path, f"{root_dir}/assets/favicon.svg")
+
+            else:
+                if not path.endswith(".gz"):
+                    os.system(f"gzip {path}")
 
         else:
             print(f"'{path}' doesn't exist...")
@@ -20,7 +26,12 @@ def replace_index_entries(index_content, paths):
 
     for path in paths:
         print(f"Processing '{path}'...")
-        index_content = index_content.replace(path, f"{path}.gz")
+
+        if "favicon.svg" in path:
+            index_content = index_content.replace(path, f"/assets/favicon.svg")
+
+        else:
+            index_content = index_content.replace(path, f"{path}.gz")
 
     return index_content
 
@@ -36,13 +47,13 @@ def main():
             index_content = index_file.read()
 
         scripts_matches = re.findall("<script.*?src=\"(.*?)\"", index_content)
-        stylesheets_matches = re.findall("<link.*?href=\"(.*?)\"", index_content)
+        resources_matches = re.findall("<link.*?href=\"(.*?)\"", index_content)
 
-        compress_files(build_directory, scripts_matches)
-        compress_files(build_directory, stylesheets_matches)
+        process_files(build_directory, scripts_matches)
+        process_files(build_directory, resources_matches)
         
         index_content = replace_index_entries(index_content, scripts_matches)
-        index_content = replace_index_entries(index_content, stylesheets_matches)
+        index_content = replace_index_entries(index_content, resources_matches)
         
         with open(os.path.join(build_directory, "index.html"), "w") as index_file:
             index_file.write(index_content)
