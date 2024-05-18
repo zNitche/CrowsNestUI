d18a9ebc5c00e87b9c7731b5e19f7d2b73886673
zNitche <53448802+zNitche@users.noreply.github.com>
2024-05-18 18:10:18 +0200

    styling cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
-import Header from "./components/header/header";
 import Home from "./pages/home";
 
 function App() {
     return (
         <div className="main-wrapper">
-            <Header />
             <div className="content-wrapper">
                 <Home />
             </div>
--- src/components/header/header.tsx
+++
@@ -1,5 +0,0 @@
-import classes from "./header.module.css";
-
-export default function Header() {
-    return <div className={classes["header-wrapper"]}>CrowsNest</div>;
-}
