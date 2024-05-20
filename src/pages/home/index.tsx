9531491a539337a17b047a06887e37f1e3c1e2a2
zNitche <53448802+zNitche@users.noreply.github.com>
2024-05-20 22:33:06 +0200

    fetching data implementation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,24 +2,55 @@ import HeaderBar from "@/components/header_bar/header_bar";
 import classes from "./index.module.css";
 import { useState } from "react";
 import ServiceTile from "@/components/service_tile/service_tile";
+import useGetServices from "@/hooks/fetch/use_get_services";
+import Loader from "@/components/design/loader/loader";
+import ServiceData from "@/types/service_data";
+import useGetSensorsData from "@/hooks/fetch/use_get_sensors_data";
 
 export default function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+
+    const {
+        isLoading: isLoadingServices,
+        isError: errorWhileLoadingServices,
+        data: services,
+    } = useGetServices(searchQuery);
+
+    const { data: sensorsData } = useGetSensorsData();
 
     return (
         <div className={classes.wrapper}>
             <HeaderBar
                 searchValue={searchQuery}
                 setSearchValue={setSearchQuery}
-                envData={{ temperature: 24, humidity: 65 }}
+                envData={{
+                    temperature: sensorsData ? sensorsData.temperature : 0,
+                    humidity: sensorsData ? sensorsData.humidity : 0,
+                }}
             />
             <div className={classes["content-container"]}>
-                <ServiceTile
-                    name={"ao3"}
-                    addedAt={new Date().toISOString()}
-                    redirectUrl={"#"}
-                    isAvailable={true}
-                />
+                {errorWhileLoadingServices && !isLoadingServices && (
+                    <div>Error while loading services data</div>
+                )}
+                {!errorWhileLoadingServices && isLoadingServices && (
+                    <div className={classes.loader}>
+                        <Loader />
+                    </div>
+                )}
+                {!errorWhileLoadingServices &&
+                    !isLoadingServices &&
+                    services !== undefined &&
+                    services.map((service: ServiceData, index) => {
+                        return (
+                            <ServiceTile
+                                key={index}
+                                name={service.name}
+                                addedAt={service.added_at}
+                                redirectUrl={service.redirect_url}
+                                isAvailable={service.is_available}
+                            />
+                        );
+                    })}
             </div>
         </div>
     );
